22cd23f330a7443d2ca9648af1ba90897dbadf38
chuaqian <148183484+chuaqian@users.noreply.github.com>
2024-08-05 23:09:20 +0800

    Added all the classes as well as my own Main code as QiAn.cs

---
+++ SWADAssgClasses/SWADAssgClasses/Accident.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWADAssgClasses
+{
+    public class Accident
+    {
+        public string AccidentId { get; set; }
+        public DateTime AccidentDate { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string ResolvingDetails { get; set; }
+    }
+}
